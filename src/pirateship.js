039f359820a1b1aa21a401de649ab93d8acaf328
Axel Hernández Ferrera <axelhzf@gmail.com>
2015-04-13 22:35:53 +0100

    change proxy url

--- src/pirateship.js
+++ src/pirateship.js
@@ -7,7 +7,7 @@ var TITLE_PREFIX = 'Details for ';
 exports.find = function (q, cb) {
   var encodeQuery = encodeURIComponent(q);
 
-  var url = 'http://baytorrent.nl/search/' + encodeQuery + '/0/7/0';
+  var url = 'https://pirateproxy.sx/search/' + encodeQuery + '/0/7/0';
 
   var options = {
     url: url,
